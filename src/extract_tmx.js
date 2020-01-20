7aa7061d6c62356dbde0f8c036ccada2600556fd
alexis <alexis.doualle@wezen.com>
2020-01-20 17:56:13 +0100

    fixed tm import script

--- src/extract_tmx.js
+++ src/extract_tmx.js
@@ -1,53 +1,63 @@
 const fs = require('fs');
 const XmlStream = require('xml-stream');
-​
+
 var myArgs = process.argv.slice(2);
-if (myArgs.length < 4) { console.log(`Syntax: node TMXExtract.js projectDirName sourceLang TargetLang tmxPath [... other tmx files]
+if (myArgs.length < 2) { console.log(`Syntax: node TMXExtract.js sourceLang targetLang
 example:
-​
-node TMXExtract.js MF en fr TMs/studio.tmx Tms/otherTM.tmx`); return }
-console.log(myArgs);
-​
-​
+
+node TMXExtract.js MF en fr data/tmx/`); return }
+
 /**
  * Extract source and target from Translation Memories for MT training
  * 1 TM segment => 1 plain text line in each file (source and target)
  */
-​
-var dirName = myArgs[0]
-var sourceLanguage = myArgs[1]
-var targetLanguage = myArgs[2]
-​
-var tmxFileList = myArgs.slice(3)
-tmxFileList.forEach(file => {
-​
-  var tmxFilePath = file
+var dirName = 'data'
+var sourceLanguage = myArgs[0]
+var targetLanguage = myArgs[1]
+var tmxFileList;
+
+tmxFileList = fs.readdirSync('data/tmx/'); 
+console.log(tmxFileList);
+
+// Create directory:
+if (!fs.existsSync('data/txt/' )){
+  fs.mkdirSync('data/txt');
+}
+
+
+tmxFileList.forEach(fileName => {
+
+  // var tmxFilePath = fileName
+  // console.log(fileName);
   
-  if (!tmxFilePath.endsWith('.tmx')) {console.log('Files needs to end with .tmx'); return}
-  tmxFilePath = tmxFilePath.substring(0, tmxFilePath.length - 4)
-​
-  // Create directory:
-  if (!fs.existsSync('./' + dirName)){
-    fs.mkdirSync('./' + dirName);
-  }
-​
-  var stream = fs.createReadStream(tmxFilePath + '.tmx');
+  
+  if (!fileName.endsWith('.tmx')) {console.log('Files needs to end with .tmx'); return}
+  fileNameWithoutExtension = fileName.substring(0, fileName.length - 4)
+  console.log(fileNameWithoutExtension);
+  
+
+  var stream = fs.createReadStream( dirName+'/tmx/'+ fileNameWithoutExtension + '.tmx');
   var xml = new XmlStream(stream);
   xml.collect('tuv');
-​
-  var endFilePath = dirName+'/'+ tmxFilePath.match(/(?!.*\/)(.*)/)[0];
-​
-  fs.writeFile(endFilePath + '.' + sourceLanguage, 'w', () => {})
-  fs.writeFile(endFilePath + '.' + targetLanguage, 'w', () => {})
-​
+
+  var endFilePath = dirName+'/txt/'+ fileNameWithoutExtension.match(/(?!.*\/)(.*)/)[0];
+  console.log(endFilePath);
+  console.log("");
+  
+  fs.writeFile(endFilePath + '.' + sourceLanguage, '', () => {})
+  fs.writeFile(endFilePath + '.' + targetLanguage, '', () => {})
+
   let stream1 = fs.createWriteStream(endFilePath + '.' + sourceLanguage, {flags: 'a'})
   let stream2 = fs.createWriteStream(endFilePath + '.' + targetLanguage, {flags: 'a'})
   let i = 0
-​
+
   xml.on('endElement: tu', function(item) {
     var efp = endFilePath
-    stream1.write(item.tuv[0].seg + '\n');
-    stream2.write(item.tuv[1].seg + '\n');
+    if (typeof item.tuv[0].seg == 'string') {
+      stream1.write(item.tuv[0].seg + '\n');
+      stream2.write(item.tuv[1].seg + '\n');
+    }
+    
     i++
     if (i%1000 === 0) {
       process.stdout.clearLine();
@@ -62,62 +72,3 @@ tmxFileList.forEach(file => {
     process.stdout.write("\n")
   })
 })
-​
-// Dior Example:
-// <?xml version="1.0" encoding="utf-8"?>
-// <tmx version="1.4">
-//   <header creationtool="SDL Language Platform" creationtoolversion="8.0" o-tmf="SDL TM8 Format" datatype="xml" segtype="sentence" adminlang="fr-FR" srclang="fr-FR" creationdate="20140903T145309Z" creationid="irina">
-//     <prop type="x-Status:MultiplePicklist">Approved,New,Read Only</prop>
-//     <prop type="x-Text Field:MultipleString"></prop>
-//     <prop type="x-Recognizers">RecognizeAll</prop>
-//     <prop type="x-TMName">DIOR_FR_UK</prop>
-//     <prop type="x-TokenizerFlags">DefaultFlags</prop>
-//     <prop type="x-WordCountFlags">AllFlags, BreakOnApostrophe</prop>
-//   </header>
-//   <body>
-//     <tu creationdate="20100730T095222Z" creationid="NICHOLAS" changedate="20100929T143902Z" changeid="DATASIA\iboukhtoiarova" lastusagedate="20101227T181136Z" usagecount="5">
-//       <prop type="x-Context">0, 0</prop>
-//       <prop type="x-Origin">TM</prop>
-//       <prop type="x-OriginalFormat">TradosTranslatorsWorkbench</prop>
-//       <tuv xml:lang="fr-FR">
-//         <seg>Timepieces</seg>
-//       </tuv>
-//       <tuv xml:lang="en-GB">
-//         <seg>Timepieces</seg>
-//       </tuv>
-//     </tu>
-//     <tu creationdate="20100129T100053Z" creationid="JENNIFER_K" changedate="20100129T100053Z" changeid="JENNIFER_K" lastusagedate="20100129T100053Z">
-//       <prop type="x-Origin">TM</prop>
-//       <prop type="x-OriginalFormat">TradosTranslatorsWorkbench</prop>
-//       <tuv xml:lang="fr-FR">
-//         <seg>MS0APUL1 001</seg>
-//       </tuv>
-//       <tuv xml:lang="en-GB">
-//         <seg>MS0APUL1 001</seg>
-//       </tuv>
-//     </tu>
-​
-​
-// MF Example:
-// <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-// <tmx>
-//     <header/>
-//     <body>
-//         <tu usagecount="2" lastusagedate="20160816T155715Z" creationdate="20160807T152804Z" creationid="DATASIA\agrasset" changedate="20160816T155715Z" changeid="DATASIA\agrasset">
-//             <prop type="x-ConfirmationLevel">ApprovedTranslation</prop>
-//             <tuv xml:lang="en-GB">
-//                 <seg>Try it with a leather biker jacket and skinny jeans on weekends.</seg>
-//             </tuv>
-//             <tuv xml:lang="fr-FR">
-//                 <seg>Associez-le à une veste motard en cuir et un jean skinny le week-end.</seg>
-//             </tuv>
-//         </tu>
-//         <tu usagecount="2" lastusagedate="20160823T083223Z" creationdate="20160807T153102Z" creationid="DATASIA\agrasset" changedate="20160823T083223Z" changeid="aurore grasset">
-//             <prop type="x-ConfirmationLevel">ApprovedTranslation</prop>
-//             <tuv xml:lang="en-GB">
-//                 <seg>Model is 5ft 10in/ 1.78m, and wears a size 3.</seg>
-//             </tuv>
-//             <tuv xml:lang="fr-FR">
-//                 <seg>Le mannequin mesure 1,78 m et porte une taille 3.</seg>
-//             </tuv>
-//         </tu>
