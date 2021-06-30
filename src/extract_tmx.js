601e32a224d7a69351f34211bb610690d09dc935
Alexis <adoualle@gmail.com>
2021-06-30 14:09:12 +0200

    code cleaning and fixes

--- src/extract_tmx.js
+++ src/extract_tmx.js
@@ -17,9 +17,6 @@ var sourceLanguage = myArgs[0]
 var targetLanguage = myArgs[1]
 var tmxFileList;
 
-// tmxFileList = fs.readdirSync('./data'); 
-// console.log(tmxFileList);
-
 // return
 tmxFileList = fs.readdirSync(dirNameToExtract); 
 console.log(tmxFileList);
@@ -54,19 +51,13 @@ tmxFileList.forEach(fileName => {
     var efp = endFilePath
     // Language check
     if (item.tuv[0].$['xml:lang'].substring(0,2) == sourceLanguage && item.tuv[1].$['xml:lang'].substring(0,2) == targetLanguage) {
-      if (typeof item.tuv[0].seg == 'string') {
-        // No duplicates
-        if (record[item.tuv[0].seg.hashCode()]) return;
-        stream1.write(item.tuv[0].seg + '\n');
-        stream2.write(item.tuv[1].seg + '\n');
-        record[item.tuv[0].seg.hashCode()] = 1;
-      } else {
-        // TU contains bpt, get $text value
-        if (record[item.tuv[0].seg.$text]) return;
-        stream1.write(item.tuv[0].seg.$text + '\n');
-        stream2.write(item.tuv[1].seg.$text + '\n');
-        record[item.tuv[0].seg.$text.hashCode()] = 1;
-      }
+      let sourceSegment = item.tuv[0].seg.$text || item.tuv[0].seg;
+      let targetSegment = item.tuv[1].seg.$text || item.tuv[1].seg;
+        // Check for duplicates
+        if (record[sourceSegment.hashCode()]) return;
+        stream1.write(sourceSegment + '\n');
+        stream2.write(targetSegment + '\n');
+        record[sourceSegment.hashCode()] = 1;
     }
     
     i++
@@ -79,7 +70,7 @@ tmxFileList.forEach(fileName => {
     }
   });
   xml.on('end', (item) => {
-    // clearLine may cause problem in docker container
+    // clearLine may cause problems in a docker container
     if (process.stdout.clearLine) {
       process.stdout.clearLine();
       process.stdout.cursorTo(0);
